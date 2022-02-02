75b2ccf266981eb5c14b9a3a456903388cb6447c
emberati <alex.shak55@gmail.com>
2022-02-03 02:25:39 +0300

    Включена политика CORS в `__init__.py`

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Config
 from flask_restful import Api
 from api import test_api
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
 api = Api(app)
+cors = CORS(app)
 
 api.add_resource(test_api.Usercacahe, '/usercache')
 
